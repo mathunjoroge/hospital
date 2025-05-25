91db1bc0cc4c543eb814b55daf3e80e2367dcdb8
mathunjoroge <mathunjoroge@gmail.com>
2025-05-25 07:09:26 +0300

    removed redudunt files

--- departments/nlp/test_spacy.py
+++
@@ -1,5 +0,0 @@
-import spacy
-nlp = spacy.load("en_core_sci_sm")
-nlp.add_pipe("scispacy_linker", config={"linker_name": "umls"})
-doc = nlp("Test text")
-print(doc.ents)
--- extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-# Database instance
-db = SQLAlchemy()
-
-# Login manager instance
-login_manager = LoginManager()
