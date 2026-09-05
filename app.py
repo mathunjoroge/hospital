0e3074f9906c6bcad58d07ccde8b560067baac18
mathunjoroge <mathunjoroge@gmail.com>
2026-09-05 22:48:51 +0300

    Fix import sorting to pass ruff linter (I001)

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ from flask_apscheduler import APScheduler
 from flask_login import current_user, login_required, login_user, logout_user
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_session import Session
 from markupsafe import Markup, escape
 from werkzeug.security import check_password_hash
 
@@ -28,7 +29,6 @@ from departments.models.admin import Log
 from departments.models.nursing import Notifications
 from departments.models.user import User
 from extensions import csrf, db, jwt, limiter, login_manager, socketio
-from flask_session import Session
 
 dotenv.load_dotenv()
 
@@ -146,7 +146,7 @@ logger.debug(f"Final Session configuration: TYPE={app.config['SESSION_TYPE']}, R
 def nl2br_safe(text):
     if text is None:
         return ''
-    return Markup('<br>'.join(escape(str(text)).split('\n')))
+    return Markup('<br>'.join(escape(str(text)).split('\n')))  # nosec B704
 
 @app.template_filter('parse_iso')
 def parse_iso(timestamp):
@@ -395,5 +395,4 @@ if __name__ == '__main__':
 
     debug_mode = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
     socketio.run(app, debug=debug_mode)
-
 
