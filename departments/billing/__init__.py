30a84d44251b22e365b740ef981224a36438c3be
mathunjoroge <mathunjoroge@gmail.com>
2026-09-10 10:24:03 +0300

    fix: move event_listeners side-effect import to correct position in billing __init__

--- departments/billing/__init__.py
+++ departments/billing/__init__.py
@@ -5,3 +5,5 @@ bp = Blueprint("billing", __name__, template_folder="templates")
 
 # Import routes to register them with the blueprint
 from . import routes  # noqa: F401, E402
+import departments.billing.event_listeners  # noqa: F401, E402
+
