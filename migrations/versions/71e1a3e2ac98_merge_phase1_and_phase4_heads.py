1600f51418b0f96a5bcc48e034d0c6bddcfe1d7f
mathunjoroge <mathunjoroge@gmail.com>
2026-09-11 23:13:43 +0300

    Add HL7 source tracking migrations and frozen requirements for Phase 4 HL7v2 MLLP Interface

---
+++ migrations/versions/71e1a3e2ac98_merge_phase1_and_phase4_heads.py
@@ -0,0 +1,24 @@
+"""merge phase1 and phase4 heads
+
+Revision ID: 71e1a3e2ac98
+Revises: 3b731701d177, 9f8g7h6i5j4k
+Create Date: 2026-09-11 22:55:25.951585
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '71e1a3e2ac98'
+down_revision = ('3b731701d177', '9f8g7h6i5j4k')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
